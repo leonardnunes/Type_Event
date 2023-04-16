5a2d753d80c98ca21e0cec817f8c901a552c94f3
Leonardo Nunes <leonardo21.nunes@gmail.com>
2023-04-16 03:22:01 -0300

    Projeto Finalizado

---
+++ cliente/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from eventos.models import Certificado
+
+
+def meus_certificados(request):
+    certificados = Certificado.objects.filter(participante=request.user)
+    return render(request, 'meus_certificados.html', {'certificados': certificados})
---
+++ usuarios/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages, auth
+from django.contrib.messages import constants
+
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+
+        if not (senha == confirmar_senha):
+            messages.add_message(request, constants.ERROR,'As senhas não coincidem.')
+            return redirect(reverse('cadastro'))
+
+        user = User.objects.filter(username=username)
+
+        if user.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário já existe.')
+            return redirect(reverse('cadastro'))
+
+        User.objects.create_user(username=username, email=email, password=senha)
+        messages.add_message(request, constants.SUCCESS, 'Salvo com sucesso!.')
+       # user.save()
+
+        return redirect(reverse('login'))
+
+def login(request):
+    if request.method == "GET":
+      return render(request, 'login.html')
+
+    elif request .method=="POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=username, password=senha)
+        if not user:
+            messages.add_message(request,constants.ERROR, 'Username ou senha inválidos.')
+            return redirect(reverse('login'))
+
+        auth.login(request,user)
+        return redirect('/eventos/novo_evento/')
+
+
+def sair(request):
+    if request.method == "GET":
+      return render(request, 'login.html')
